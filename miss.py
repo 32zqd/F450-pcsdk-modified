c4b80f7e611a97043c72be5ce84e9208eb6aaa2b
baiye <1772869816@qq.com>
2023-09-11 02:41:13 +0000

    update miss.py.
    Signed-off-by: baiye <1772869816@qq.com>

--- miss.py
+++ miss.py
@@ -3,6 +3,7 @@ import math
 from mavsdk.offboard import PositionNedYaw
 from mavsdk import System, mission
 
+#END坐标，高度为-数，以下程序做了修改，正常数输入即可
 
 async def run():
     # 连接无人机
@@ -68,7 +69,7 @@ async def run():
     
     while True:
 
-        input_str = input("输入X轴,Y轴,高度,速度，悬停时间，航向角\n(注意：高度为相对高度，共6个参数，注意参数描述，高度输入负数意味着低于起飞高度,且需要两个航点任务)   空格隔开\n ")
+        input_str = input("输入北东地（地已转换正数）--X轴,Y轴,高度,速度，悬停时间，航向角\n(注意：高度为相对高度，共6个参数，注意参数描述，高度输入负数意味着低于起飞高度,且需要两个航点任务)   空格隔开\n ")
         try:
             x,y,z,s,time_1, yaw_1 = map(float, input_str.split( ))
         except ValueError:
